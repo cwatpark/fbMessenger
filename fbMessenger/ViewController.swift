f78b4971f1b1183758db5399f72f58dfc11dac6a
cwatpark <chaiwat.sob@spumail.net>
2019-02-11 12:13:43 +0700

    Initial Commit

---
+++ fbMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fbMessenger
+//
+//  Created by Ice on 11/2/2562 BE.
+//  Copyright © 2562 Ice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
